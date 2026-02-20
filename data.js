38cdef777fb6b1b8be9fcfb0d9a4fefa317dcb8c
DbaraTounsia <eldbaratounsia@gmail.com>
2026-02-20 16:14:48 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const foodData = {
+  soup: [
+    {name: "Chorba Frik", img: "https://www.lapetitecuisinedenat.com/wp-content/uploads/2023/01/chorba-frik1.jpg"},
+    {name: "Soupe Lentille", img: "https://content.joseedistasio.ca/app/uploads/2019/06/05145718/soupe_indienne_lentilles_rouges-josee_di_stasio.jpeg"},
+    {name: "Langue d’Oiseau", img: "https://www.ptitchef.com/imgupl/recipe/chorba-aux-langues-d-oiseaux--lg-133900p201443.webp"}
+  ],
+
+  Salade: [
+    {name: "Salade Tunisienne", img: "https://miyado-cuisine.tn/wp-content/uploads/2021/02/salade-tunisienne-min.jpg"},
+    {name: "Salade Mechouia", img: "https://img.cuisineaz.com/660x660/2024/08/16/i199460-salade-mechouia-tunisienne.jpg"}
+  ],
+
+  starter: [
+    {name: "Brik", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0jDM8qVmoOfqDCrvUzEUQdlcL4pob3weA3w&s"},
+    {name: "Mini Pizza", img: "https://theclevermeal.com/wp-content/uploads/2024/11/minipizzas_4.jpg"}
+  ],
+
+  main: [
+    {name: "Ojja", img: "https://i0.wp.com/lesjoyauxdesherazade.com/wp-content/uploads/2014/05/ojja-aux-merguez.jpg"},
+    {name: "Couscous", img: "https://assets.afcdn.com/recipe/20200528/111567_w1024h768c1cx540cy360.jpg"}
+  ],
+
+  Dessert: [
+    {name: "Baklava", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYq6nBaW8rAHInfcCBgPuoT7brg4TKKNicw&s"},
+    {name: "Makroud", img: "https://patisseriemasmoudi.fr/cdn/shop/products/Makhroudhasmar_dc744b0b-7a9b-42fa-ba51-82521a558725_grande.jpg?v=1757076080"}
+  ]
+};
